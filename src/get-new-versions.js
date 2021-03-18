4eca31e5bf764880f139c1c197cad9ef20c797bd
Kelly Selden <kellyselden@gmail.com>
2021-03-18 10:14:14 +0000

    chore: fix typo

--- src/get-new-versions.js
+++ src/get-new-versions.js
@@ -5,7 +5,7 @@ const {
 } = require('./git');
 const buildDepGraph = require('./build-dep-graph');
 const buildChangeGraph = require('./build-change-graph');
-const _buildReleaseGraph = require('./build-release-graph');
+const buildReleaseGraph = require('./build-release-graph');
 const semver = require('semver');
 
 const { builder } = require('../bin/commands/release');
@@ -29,7 +29,7 @@ async function getNewVersions({
     cached,
   });
 
-  let releaseTrees = await _buildReleaseGraph({
+  let releaseTrees = await buildReleaseGraph({
     packagesWithChanges,
     shouldBumpInRangeDependencies,
     shouldInheritGreaterReleaseType,
