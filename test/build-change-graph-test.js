2d31545307535ff99fd141d3ee7dbadd46063feb
Kelly Selden <kellyselden@gmail.com>
2021-03-18 10:52:29 +0000

    chore: use `some` instead of `filter`

--- test/build-change-graph-test.js
+++ test/build-change-graph-test.js
@@ -821,9 +821,9 @@ describe(buildChangeGraph, function() {
     let packagesWithChanges = await buildChangeGraph({ workspaceMeta });
 
     expect(packagesWithChanges).to.match(this.match(packagesWithChanges => {
-      return !packagesWithChanges.filter(pkg => {
+      return !packagesWithChanges.some(pkg => {
         return pkg.dag.packageName === 'root';
-      }).length;
+      });
     }));
   });
 });
